6bd919e8a8e817c90f9bdbf7f998b533934ab371
idempotencecloud <alex@idempotence.io>
2023-07-31 12:53:19 -0500

    Add eslintint

---
+++ .eslintrc.js
@@ -0,0 +1,12 @@
+module.exports = {
+  root: true,
+  env: {
+    node: true,
+  },
+  extends: [  'eslint:recommended',
+  "plugin:vue/vue3-recommended",
+  "prettier"],
+  rules: {
+    // Add any additional ESLint rules or overrides here
+  },
+};
