963b842ff459e4e3a7d2e8085706ff86d30e55cf
idempotencecloud <alex@idempotence.io>
2023-07-31 12:39:57 -0500

    Refactor individual form in to its own file.

---
+++ src/components/InidividualInfoForm.vue
@@ -0,0 +1,89 @@
+<template>
+          <div class="mx-auto max-w-2xl text-center">
+        <h2 class="text-3xl font-bold tracking-tight text-gray-900 sm:text-4xl">Company Agent Onboarding</h2>
+        <p class="mt-2 text-lg leading-8 text-gray-600">Please fill the form belolw, to onboard yourself as a company agent.</p>
+      </div>
+      <form action="#" method="POST" class="mx-auto mt-16 max-w-xl sm:mt-20">
+        <div class="grid grid-cols-1 gap-x-8 gap-y-6 sm:grid-cols-2">
+          <div>
+            <label for="first-name" class="block text-sm font-semibold leading-6 text-gray-900">First name</label>
+            <div class="mt-2.5">
+              <input type="text" name="first-name" id="first-name" autocomplete="given-name" class="block w-full rounded-md border-0 px-3.5 py-2 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6" />
+            </div>
+          </div>
+          <div>
+            <label for="last-name" class="block text-sm font-semibold leading-6 text-gray-900">Last name</label>
+            <div class="mt-2.5">
+              <input type="text" name="last-name" id="last-name" autocomplete="family-name" class="block w-full rounded-md border-0 px-3.5 py-2 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6" />
+            </div>
+          </div>
+          <div class="sm:col-span-2">
+            <label for="drivers-lisence" class="block text-sm font-semibold leading-6 text-gray-900">Driver&#x27;s Lisence Number</label>
+            <div class="relative mt-2.5">
+              <div class="absolute inset-y-0 left-0 flex items-center">
+                <label for="dl-state" class="sr-only">DL State</label>
+                <select id="dl-state" name="dl-state" class="h-full rounded-md border-0 bg-transparent bg-none py-0 pl-4 pr-9 text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm">
+                  <option v-for="state in usStates" :key="state.name">{{ state.abbreviation }}</option>
+                </select>
+              </div>
+              <input type="text" name="drivers-lisence" id="drivers-lisence" autocomplete="off" class="block w-full rounded-md border-0 px-3.5 py-2 pl-28 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6" />
+            </div>
+          </div>
+          <div class="sm:col-span-2">
+            <label for="email" class="block text-sm font-semibold leading-6 text-gray-900">Email</label>
+            <div class="mt-2.5">
+              <input type="email" name="email" id="email" autocomplete="email" class="block w-full rounded-md border-0 px-3.5 py-2 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6" />
+            </div>
+          </div>
+          <div>
+            <label for="password" class="block text-sm font-semibold leading-6 text-gray-900">Password</label>
+            <div class="mt-2.5">
+              <input type="password" name="password" id="password" class="block w-full rounded-md border-0 px-3.5 py-2 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6" />
+            </div>
+          </div>
+          <div>
+            <label for="confirm-password" class="block text-sm font-semibold leading-6 text-gray-900">Confirm Password</label>
+            <div class="mt-2.5">
+              <input type="password" name="confirm-password" id="confirm-password" class="block w-full rounded-md border-0 px-3.5 py-2 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6" />
+            </div>
+          </div>
+          <div class="sm:col-span-2">
+            <label for="phone-number" class="block text-sm font-semibold leading-6 text-gray-900">Phone number</label>
+            <div class="relative mt-2.5">
+              <input type="tel" name="phone-number" id="phone-number" placeholder="+1 (555) 987-6543" autocomplete="tel" class="block w-full rounded-md border-0 px-3.5 py-2 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6" />
+            </div>
+          </div>
+          <div class="sm:col-span-2">
+            <label for="mailing-address-1" class="block text-sm font-semibold leading-6 text-gray-900">Mailing Address Line 1</label>
+              <input type="text" name="mailing-address-1" id="mailing-address-1" autocomplete="off" class="block w-full rounded-md border-0 px-3.5 py-2 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6" />
+          </div>
+          <div class="sm:col-span-2">
+            <label for="mailing-address-2" class="block text-sm font-semibold leading-6 text-gray-900">Mailing Address Line 2</label>
+              <input type="text" name="mailing-address-2" id="mailing-address-2" autocomplete="off" class="block w-full rounded-md border-0 px-3.5 py-2 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6" />
+          </div>
+          <div class="sm:col-span-2">
+            <label for="city" class="block text-sm font-semibold leading-6 text-gray-900">City</label>
+              <input type="text" name="city" id="city" autocomplete="off" class="block w-full rounded-md border-0 px-3.5 py-2 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6" />
+          </div>
+          <div class="sm:col-span-2">
+            <label for="state" class="block text-sm font-semibold leading-6 text-gray-900">State</label>
+            <div class="relative mt-2.5">
+              <div class="absolute inset-y-0 left-0 flex items-center">
+                <label for="state" class="sr-only">State</label>
+                <select id="state" name="state" class="h-full rounded-md border-0 bg-transparent bg-none py-0 pl-4 pr-9 text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm">
+                  <option v-for="state in usStates" :key="state.name">{{ state.abbreviation }}</option>
+                </select>
+              </div>
+              <input placeholder="Zip Code" type="text" name="drivers-lisence" id="drivers-lisence" autocomplete="off" class="block w-full rounded-md border-0 px-3.5 py-2 pl-28 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6" />
+            </div>
+          </div>
+        </div>
+        <div class="mt-10">
+          <button type="submit" class="block w-full rounded-md bg-indigo-600 px-3.5 py-2.5 text-center text-sm font-semibold text-white shadow-sm hover:bg-indigo-500 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-600">Proceed</button>
+        </div>
+      </form>
+  </template>
+  
+  <script setup>
+  import { usStates } from '../data/states.js';
+  </script>
