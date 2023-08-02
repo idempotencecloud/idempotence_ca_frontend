5b8c0be50dc128d1ab5e6bf6efd20d9221fbf262
idempotencecloud <alex@idempotence.io>
2023-08-02 10:41:04 -0500

    Add VScode eslint options.

--- .eslintrc.js
+++ .eslintrc.js
@@ -3,10 +3,13 @@ module.exports = {
   env: {
     node: true,
   },
-  extends: [  'eslint:recommended',
-  "plugin:vue/vue3-recommended",
-  "prettier"],
+  extends: ['plugin:vue/vue3-essential', 'eslint:recommended', 'prettier'],
+  // Remove the "parser" key
+  parserOptions: {
+    // Update the parser to use the built-in parser instead of babel-eslint
+    parser: '@babel/eslint-parser',
+  },
   rules: {
-    // Add any additional ESLint rules or overrides here
+    // Add any additional rules or overrides here
   },
 };
