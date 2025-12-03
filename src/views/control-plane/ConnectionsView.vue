5ffb8d66d78953fc2f2499ddddb7a520ab7b7206
idempotencecloud <alex@idempotence.io>
2025-12-03 02:34:15 -0600

    plus sign

--- src/views/control-plane/ConnectionsView.vue
+++ src/views/control-plane/ConnectionsView.vue
@@ -178,7 +178,7 @@
                     class="block w-full rounded-md border-0 px-3.5 py-2 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6"
                   />
                   <small class="text-gray-400"
-                    >Using a plus sign with the e-mail could lead to, or solve, multiple
+                    >Using a plus sign %28%2B%29 with the e-mail could lead to, or solve, multiple
                     organizations being created.</small
                   >
                 </div>
